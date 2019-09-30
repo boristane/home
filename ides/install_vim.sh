959c25bb4e8712cf75fad83f14dfc9014bd0c35d
Boris Tane <boris.tane@gmail.com>
2019-09-30 11:24:02 +0100

    Adds vim

---
+++ ides/install_vim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+bold=`tput bold`
+
+function install_vim() {
+  echo "${green}${bold}Installing vim${reset}"
+  apt-get install -y vim
+  if [[ -z $(grep 'export VISUAL=vim'  ${HOME}/.bashrc) ]]; then
+    echo 'export VISUAL=vim' >> ${HOME}/.bashrc
+  fi
+  if [[ -z $(grep 'export EDITOR=$VISUAL'  ${HOME}/.bashrc) ]]; then
+    echo 'export EDITOR=$VISUAL' >> ${HOME}/.bashrc
+  fi
+  echo "${green}${bold}Succesfully installed vim${reset}"
+}
+
+install_vim
