820c982a1e703df6fc0bbfa59219252013c1ed7d
Boris Tane <boris.tane@gmail.com>
2019-10-26 14:21:51 +0100

    Set the docker-composer version to 1.24.0

--- docker/install_docker_compose.sh
+++ docker/install_docker_compose.sh
@@ -7,7 +7,8 @@ bold=`tput bold`
 
 function install_docker_compose() {
   echo -e "${green}${bold}Installing Docker Compose${reset}"
-  curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
+  version=1.24.0
+  curl -L https://github.com/docker/compose/releases/download/${version}/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
   chmod +x /usr/bin/docker-compose
   echo -e "${green}${bold}Succesfully installed `/usr/bin/docker-compose -v`${reset}"
 }
